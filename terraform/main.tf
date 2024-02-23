a3a7b6f7ebbd44ce5ff712b900d7ddfd8bd81dc9
bonetou <h.boneto@outlook.com>
2024-02-23 00:01:02 -0300

    infra: change to cloud run v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,19 +3,25 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_cloud_run_service" "crawler_api" {
+
+resource "google_cloud_run_v2_service" "crawler_api" {
   name     = "crawler-api"
-  location = var.region
+  location = "us-central1"
+  ingress = "INGRESS_TRAFFIC_ALL"
+
   template {
-    spec {
-      containers {
-        image = var.cloud_run_image
+    containers {
+      image = var.cloud_run_image
+      resources {
+        limits = {
+          cpu    = "2"
+          memory = "1024Mi"
+        }
       }
     }
   }
   traffic {
-    percent         = 100
-    latest_revision = true
+    percent = 100
   }
 }
 
@@ -27,6 +33,6 @@ resource "google_pubsub_subscription" "crawling_started_subscription" {
   name   = "crawling-started-subscription"
   topic  = google_pubsub_topic.crawling_started_topic.name
   push_config {
-    push_endpoint = "${google_cloud_run_service.crawler_api.status[0].url}/internal/process"
+    push_endpoint = "${google_cloud_run_v2_service.crawler_api.uri}/internal/process"
   }
 }
