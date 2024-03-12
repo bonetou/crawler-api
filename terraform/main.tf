5846023cd01b73a134548915170e394a051dffe9
bonetou <h.boneto@outlook.com>
2024-03-11 22:53:59 -0300

    infra: make cloud run public

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,18 +4,17 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_artifact_registry_repository" "images-repository" {
+resource "google_artifact_registry_repository" "images_repository" {
   location      = var.region
   repository_id = "images"
   description   = "A repository for storing images"
   format        = "DOCKER"
 }
 
-resource "google_cloud_run_v2_service" "crawler-api" {
+resource "google_cloud_run_v2_service" "crawler_api" {
   name     = "crawler-api"
   location = var.region
-  ingress = "INGRESS_TRAFFIC_ALL"
-
+  
   template {
     containers {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
@@ -28,16 +27,40 @@ resource "google_cloud_run_v2_service" "crawler-api" {
     }
   }
 }
+  
+  
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers"
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = var.region
+  project  = var.project_id
+  service  = google_cloud_run_v2_service.crawler_api.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
 
 resource "google_pubsub_topic" "crawling_started_topic" {
   name = "crawling-started"
 }
 
+resource "google_storage_bucket" "crawling_screenshots" {
+  name = "crawling-screenshots"
+  location = var.region
+  force_destroy = true
+}
+
+
 resource "google_pubsub_subscription" "crawling_started_subscription" {
   name   = "crawling-started-subscription"
   topic  = google_pubsub_topic.crawling_started_topic.name
   push_config {
-    push_endpoint = "${google_cloud_run_v2_service.crawler-api.uri}/internal/extract_links"
+    push_endpoint = "${google_cloud_run_v2_service.crawler_api.uri}/internal/extract_links"
   }
   depends_on = [ google_pubsub_topic.crawling_started_topic ]
 }
